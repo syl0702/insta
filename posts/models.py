f56fbbbcd84a626127ea4920af3571c9e96e5f5e
syl0702 <navyblue794@gmail.com>
2023-08-30 11:41:41 +0900

    resized

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django_resized import ResizedImageField
 
 # Create your models here.
 class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='image/%Y/%m')
+    # image = models.ImageField(upload_to='image/%Y/%m')
+    image = ResizedImageField(
+        size=[500,500],
+        crop=['middle', 'center'],
+        upload_to='image/%Y/%m'
+    )
