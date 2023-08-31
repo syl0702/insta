ab916c694bc5d559710b0d4893c75fb727694d6c
syl0702 <navyblue794@gmail.com>
2023-08-31 10:11:37 +0900

    comment create&read 기능구현

--- posts/models.py
+++ posts/models.py
@@ -15,3 +15,10 @@ class Post(models.Model):
     )
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     # user_id =
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all().order_by('-id')
+    comment_form = CommentForm()
     context = {
         'posts': posts,
+        'comment_form': comment_form,
     }
     return render(request, 'index.html', context)
 
@@ -26,3 +28,20 @@ def create(request):
     }
 
     return render(request, 'form.html', context)
+
+
+def comment_create(request, post_id):
+    comment_form = CommentForm(request.POST)
+    
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+
+        # 현재 로그인 유저
+        comment.user = request.user
+        # post_id를 기준으로 찾은 post
+        post = Post.objects.get(id=post_id)
+        comment.post = post
+
+        comment.save()
+
+        return redirect('posts:index')
